e03c4196da8136c45b9be1dfc2ec8cb750a37a07
Carl Norum <carl@norum.ca>
2020-06-15 13:51:36 -0700

    Stephen's second patch.

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # photolib
 
-Usage: `photolib.py SOURCE DESTINATION`
+usage: photolib.py [-h] [--verbose] source dest
 
-Where `SOURCE` is the bundle directory of the photo library and `DESTINATION` is a directory you want the results to go in.
+Extract information and photos from ancient iPhoto libraries.
+
+positional arguments:
+  source         bundle directory of the photo library source
+  dest           directory you want the results to go in
+
+optional arguments:
+  -h, --help     show this help message and exit
+  --verbose, -v  enable verbose logging
--- photolib.py
+++ photolib.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import plistlib
 import shutil
 import sys
-import argparse
 
-def create_destination_dir(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
 
-def load_album_info(path):
-    with open(os.path.join(path, "AlbumData.xml"), "rb") as f:
-        return plistlib.load(f)
+VERBOSE = False
+
+
+def verbose_log(*args, **kwargs):
+    global VERBOSE
+    if VERBOSE:
+        print(*args, **kwargs)
+
 
 # thanks StackOverflow!
 def remove_prefix(text, prefix):
@@ -20,58 +22,66 @@ def remove_prefix(text, prefix):
         return text[len(prefix):]
     return text
 
-def extract_info(source, dest, album_info):
-    create_destination_dir(dest)
 
-    # These are default/builtin iPhoto album names that don't contain
-    # meaningful information for us. Skip 'em so they don't end up in
-    # the albums index.
-    skip_albums = [
-        "Photos",
-        "Last 12 Months",
-        "Last Import",
-        "Flagged"
-    ]
+def create_destination_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
+
+def load_album_data(path):
+    with open(os.path.join(path, "AlbumData.xml"), "rb") as f:
+        return plistlib.load(f)
+
+
+def album_path_prefx(album_data):
     # iPhoto stores images with full pathnames, which is meaningless
     # if the album is ever moved, but handily stores what it thinks
     # the root directory of the album is. Bizarre.
-    path_prefix = album_info["Archive Path"] + os.sep
+    return album_data["Archive Path"] + os.sep
 
+
+def get_album_info(album_data, skip_albums):
+    path_prefix = album_path_prefx(album_data)
     # Album list and file paths out to the 'albums.txt' file
     d = {}
-    for album in album_info["List of Albums"]:
+    for album in album_data["List of Albums"]:
         name = album["AlbumName"]
         if name in skip_albums:
             continue
 
         d[name] = []
         for key in album["KeyList"]:
-            pic = album_info["Master Image List"][key]
+            pic = album_data["Master Image List"][key]
             path = remove_prefix(pic["ImagePath"], path_prefix)
             d[name].append(path)
+    return d
 
+
+def dump_album_info(dest, album_info):
+    create_destination_dir(dest)
     with open(os.path.join(dest, "albums.txt"), "w") as f:
-        for key in sorted(d.keys()):
+        for key in sorted(album_info.keys()):
             print(key, file=f)
-            for item in sorted(d[key]):
+            for item in sorted(album_info[key]):
                 print(" ", item, file=f)
             print("", file=f)
 
-    # Copy pictures over
+
+def copy_photos(source, dest, album_data):
+    path_prefix = album_path_prefx(album_data)
     i = {}
-    for pic in album_info["Master Image List"].values():
+    for pic in album_data["Master Image List"].values():
         path = remove_prefix(pic["ImagePath"], path_prefix)
 
         imagesrc = os.path.join(source, path)
 
         # A bunch of the pictures in the master image list appear to be
         # duplicated, and a swath of them had broken pathnames. I just
-        # skip them here, since it doesn't look like they matter, but
-        # uncomment this print to find things if you can't figure out
+        # skip them here, since it doesn't look like they matter. Use
+        # verbose logging to find things if you can't figure out
         # why something important vanished/wasn't copied.
         if not os.path.exists(imagesrc):
-            #print("Not found:", path)
+            verbose_log("Not found:", path)
             continue
 
         # pathname wrangling
@@ -93,9 +103,8 @@ def extract_info(source, dest, album_info):
         i[d][filename]["Comment"] = pic["Comment"]
 
     # write out indices
-    for d in i.keys():
+    for d, index in i.items():
         with open(os.path.join(dest, d, "index.txt"), "w") as f:
-            index = i[d]
             for filename in sorted(index.keys()):
                 print(filename, file=f)
                 caption = index[filename]["Caption"]
@@ -106,20 +115,43 @@ def extract_info(source, dest, album_info):
                     print(" ", comment, file=f)
                 print("", file=f)
 
+
 def process_arguments(argv):
     """Returns processed arguments."""
     parser = argparse.ArgumentParser(
         description='Extract information and photos from ancient iPhoto libraries.')
     parser.add_argument('source', help='bundle directory of the photo library source')
     parser.add_argument('dest', help='directory you want the results to go in')
+    parser.add_argument('--verbose', '-v', action='store_true', help='enable verbose logging')
     args = parser.parse_args(argv)
     return args
 
+
 def main(argv=None):
+    global VERBOSE
+
+    # These are default/builtin iPhoto album names that don't contain
+    # meaningful information for us. Skip 'em so they don't end up in
+    # the albums index.
+    skip_albums = [
+        "Photos",
+        "Last 12 Months",
+        "Last Import",
+        "Flagged"
+    ]
+
     args = process_arguments(argv)
-    album_info = load_album_info(args.source)
-    extract_info(args.source, args.dest, album_info)
+    VERBOSE = args.verbose
+
+    album_data = load_album_data(args.source)
+
+    album_info = get_album_info(album_data, skip_albums)
+    dump_album_info(args.dest, album_info)
+
+    copy_photos(args.source, args.dest, album_data)
+
     return 0
+
 
 if __name__ == '__main__':
     status = main()
